b5ed2525e741d61e83ac8e0fe415c12d88653b5c
Nik Mirza <nik96mirza@gmail.com>
2021-01-17 09:55:09 +0800

    wrong folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import BaseModel from './src/BaseModel';
 import QueryBuilder from './src/QueryBuilder';
 import DataType from './src/DataType';
-import Sqlite from './src/Sqlite';
+import Sqlite from './src/Adapter/Sqlite';
 
 
 export default {
